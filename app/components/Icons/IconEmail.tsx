34f0bfebd5a1c082de22cf050918a22c8a888c2f
gurayalinn <gurayalinn@gmail.com>
2023-07-09 23:10:45 +0300

    Icon and links changed.

---
+++ app/components/Icons/IconEmail.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+
+interface SVGRProps {
+  title?: string
+  titleId?: string
+}
+export const IconEmail = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    data-name="1-Email"
+    width="1em"
+    height="1em"
+    focusable="false"
+    aria-hidden="true"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path d="M29 4H3a3 3 0 0 0-3 3v18a3 3 0 0 0 3 3h26a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-.72 2L16 14.77 3.72 6zM30 25a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.23l13.42 9.58a1 1 0 0 0 1.16 0L30 7.23z" />
+  </svg>
+)
